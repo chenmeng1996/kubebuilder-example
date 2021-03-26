1e4191966acb71ffb35f64b02bcbfa5b7b99636b
陈濛 <chenmeng.yzy@bytedance.com>
2021-03-26 18:31:46 +0800

    安装cert-manager

---
+++ cmd/install_certmanager.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --version v1.2.0 \
+  --create-namespace \
+  --set installCRDs=true
